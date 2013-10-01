a7cb431d645cd1dc6049c95c116263a650bc4307
beto-alves <beto.alves.b@gmail.com>
2013-10-01 20:32:57 -0300

    Adicionado form para visualizacao e reimpressao de receita

---
+++ SisClin2.0/SisClin2.0/View/VisReceita.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SisClin2._0.View
+{
+    partial class VisReceita
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisReceita));
+            this.lblMedico = new System.Windows.Forms.Label();
+            this.lblPaciente = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPrescricao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMedico
+            // 
+            this.lblMedico.AutoSize = true;
+            this.lblMedico.Location = new System.Drawing.Point(122, 41);
+            this.lblMedico.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMedico.Name = "lblMedico";
+            this.lblMedico.Size = new System.Drawing.Size(46, 17);
+            this.lblMedico.TabIndex = 22;
+            this.lblMedico.Text = "label6";
+            // 
+            // lblPaciente
+            // 
+            this.lblPaciente.AutoSize = true;
+            this.lblPaciente.Location = new System.Drawing.Point(122, 12);
+            this.lblPaciente.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPaciente.Name = "lblPaciente";
+            this.lblPaciente.Size = new System.Drawing.Size(46, 17);
+            this.lblPaciente.TabIndex = 21;
+            this.lblPaciente.Text = "label5";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(122, 74);
+            this.lblData.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(46, 17);
+            this.lblData.TabIndex = 20;
+            this.lblData.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 74);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Data:";
+            // 
+            // textPrescricao
+            // 
+            this.textPrescricao.Location = new System.Drawing.Point(125, 104);
+            this.textPrescricao.Margin = new System.Windows.Forms.Padding(4);
+            this.textPrescricao.Multiline = true;
+            this.textPrescricao.Name = "textPrescricao";
+            this.textPrescricao.Size = new System.Drawing.Size(411, 172);
+            this.textPrescricao.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Prescrição:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Medico:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 18);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Paciente:";
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnImprimir.Image = ((System.Drawing.Image)(resources.GetObject("btnImprimir.Image")));
+            this.btnImprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImprimir.Location = new System.Drawing.Point(416, 284);
+            this.btnImprimir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnImprimir.Size = new System.Drawing.Size(120, 40);
+            this.btnImprimir.TabIndex = 34;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // VisReceita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 343);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.lblMedico);
+            this.Controls.Add(this.lblPaciente);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPrescricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "VisReceita";
+            this.ShowIcon = false;
+            this.Text = "Visualizar Receita";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMedico;
+        private System.Windows.Forms.Label lblPaciente;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textPrescricao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
